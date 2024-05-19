4475147dba73c6e67c86d2f9073ce8d4feaadc28
Joseph <josephayman@mail.com>
2024-05-20 00:49:09 +0200

    New website

--- cv-timeline.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const containers = document.querySelectorAll('.container');
-    containers.forEach((container, index) => {
-      container.style.setProperty('--animation-order', index + 1);
-    });
-  });
-  
